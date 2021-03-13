c5714f13caa750495687a04a0eda5df805da183e
Yeamin Hossain <yeaminhossain2@gmail.com>
2021-03-14 00:43:34 +0600

    axios

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -7,7 +7,7 @@ const axiosIns = axios.create({
   // You can add your headers here
   // ================================
   baseURL: 'http://127.0.0.1:8000',
-  timeout: 2000,
+  timeout: 5000,
   headers: { Accept: 'application/json' },
 })
 
